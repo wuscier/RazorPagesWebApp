366eee1e7a699417b713132e7df61ff18537e432
wuscier <1352288123@qq.com>
2018-03-21 14:51:50 +0800

    change display format

--- RazorPagesWebApp/Models/Movie.cs
+++ RazorPagesWebApp/Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +10,9 @@ namespace RazorPagesWebApp.Models
     {
         public int ID { get; set; }
         public string Title { get; set; }
+
+        [Display(Name="Release Date")]
+        [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
         public decimal Price { get; set; }
